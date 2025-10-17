82d8731136f91d87578062025186c66d06241f1f
Vitaliy <70775970+vitaliy-101@users.noreply.github.com>
2025-10-17 19:02:18 +0300

    изменил имя директорий

--- core/src/main/java/com/example/core/CoreApplication.java
+++ core/src/main/java/com/example/core/CoreApplication.java
@@ -1,4 +1,4 @@
-package com.example.facade;
+package com.example.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- core/src/test/java/com/example/core/CoreApplicationTests.java
+++ core/src/test/java/com/example/core/CoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.facade;
+package com.example.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
